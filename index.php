c3df9697aa7149a6c0cbcee174f7d7b66e7fc6fe
Ashutosh Agrawal <ashutosh@hackerrank.com>
2014-02-22 18:48:28 +0530

    adding website template

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Sample Website</title>
+
+		<link rel="stylesheet" type="text/css" href="_styles/style.css" />
+
+		<script src="_scripts/loader.js" ></script>
+	</head>
+	<body>
+
+		<!-- Header Start -->
+
+		<?php include_once 'header.php'; ?>
+
+		<!-- Header Ends -->
+
+
+		<!-- Page Starts -->
+
+		<?php
+
+		$url = "home";
+		if($_GET['page'])
+		{
+			$url = $_GET['page'];
+		}
+		$url = $url.".php";
+
+		include_once $url
+
+		?>
+
+		<!-- Page Ends -->
+
+
+		<!-- Footer Starts  -->
+
+		<?php include_once 'footer.php'; ?>
+
+		<!-- Footer Ends -->
+	</body>
+</html>
